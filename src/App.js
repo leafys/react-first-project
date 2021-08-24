fea0d94d39f4652d162299c42dc7dc536fc79a08
Sergey Leus <leafysprojects@gmail.com>
2021-08-24 17:38:36 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import Technologies from './technologies.js'
+import Header from './header.js';
+
+const App = () => {
+  return (
+    <div>
+      <Technologies />
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/technologies.js
@@ -0,0 +1,14 @@
+const Technologies = () => {
+    return (
+      <div>
+      <ul>
+        <li>asd</li>
+        <li>asd2</li>
+        <li>asd3</li>
+        <li>asd4</li>
+      </ul>
+    </div>
+    )
+  }
+
+  export default Technologies;
